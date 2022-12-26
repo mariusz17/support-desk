9d2f4ddf7fabef1938be135e72299aa243b65b8a
mariusz17 <qbajt.mariusz.kubat@gmail.com>
2022-12-26 13:49:34 +0100

    add display error in console in error middleware

--- backend/src/middleware/errorMiddleware.ts
+++ backend/src/middleware/errorMiddleware.ts
@@ -4,6 +4,8 @@ import { config } from "../config/config";
 const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
   const statusCode = res.statusCode ? res.statusCode : 500;
 
+  console.log(err);
+
   res.status(statusCode);
   res.json({
     message: err.message,
